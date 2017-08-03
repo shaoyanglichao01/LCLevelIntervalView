10c9c33b5d95b999d8582d902d4d4cddbe59021e
chao.li <shaoyanglichao@126.com>
2017-08-03 15:07:19 +0800

    Initial Commit

---
+++ LCLevelIntervalView/LCLevelIntervalView.h
@@ -0,0 +1,13 @@
+//
+//  LCLevelIntervalView.h
+//  LCLevelIntervalView
+//
+//  Created by lichao on 2017/8/3.
+//  Copyright © 2017年 lichao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LCLevelIntervalView : NSObject
+
+@end
---
+++ LCLevelIntervalView/LCLevelIntervalView.m
@@ -0,0 +1,13 @@
+//
+//  LCLevelIntervalView.m
+//  LCLevelIntervalView
+//
+//  Created by lichao on 2017/8/3.
+//  Copyright © 2017年 lichao. All rights reserved.
+//
+
+#import "LCLevelIntervalView.h"
+
+@implementation LCLevelIntervalView
+
+@end
