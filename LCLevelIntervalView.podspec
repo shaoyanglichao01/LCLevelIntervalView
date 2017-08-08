6ac733ad24c2d63cc69d7c25a7a43e857244c20d
chao.li <shaoyanglichao@126.com>
2017-08-08 14:45:03 +0800

    修改bugw

--- LCLevelIntervalView.podspec
+++ LCLevelIntervalView.podspec
@@ -16,8 +16,8 @@ Pod::Spec.new do |s|
   #
 
   s.name         = "LCLevelIntervalView"
-  s.version      = "0.0.5"
-  s.summary      = "更新布局"
+  s.version      = "0.0.6"
+  s.summary      = "修改bug"
 
   # This description is used to generate tags and improve search results.
   #   * Think: What does it do? Why did you write it? What is the focus?
--- LCLevelIntervalView/LCLevelIntervalView.m
+++ LCLevelIntervalView/LCLevelIntervalView.m
@@ -30,30 +30,30 @@
 }
 
 - (void)showInView:(UIView *)view {
-//  NSParameterAssert(view);
-//  
-//  if (self.showing) {
-//    return;
-//  }
-//  
-//  if (!self.maskView) {
-//    self.maskView = [UIButton buttonWithType:UIButtonTypeCustom];
-//    [self.maskView setBackgroundColor:[[UIColor blackColor] colorWithAlphaComponent:.4]];
-//    [self.maskView addTarget:self action:@selector(dismiss) forControlEvents:UIControlEventTouchUpInside];
-//  }
-//  
-//  [view addSubview:self.maskView];
-//  self.maskView.frame = view.bounds;
-//  
-//  self.frame = CGRectMake(20, OPTScreenHeight, OPTScreenWidth-40, 300);
-//  [view addSubview:self];
-//  [view layoutIfNeeded];
-//  
-//  [UIView animateWithDuration:.3 animations:^{
-//    self.center = CGPointMake(OPTScreenWidth/2.0, OPTScreenHeight/2.0);
-//  }];
-//  
-//  self.showing = YES;
+  //  NSParameterAssert(view);
+  //
+  //  if (self.showing) {
+  //    return;
+  //  }
+  //
+  //  if (!self.maskView) {
+  //    self.maskView = [UIButton buttonWithType:UIButtonTypeCustom];
+  //    [self.maskView setBackgroundColor:[[UIColor blackColor] colorWithAlphaComponent:.4]];
+  //    [self.maskView addTarget:self action:@selector(dismiss) forControlEvents:UIControlEventTouchUpInside];
+  //  }
+  //
+  //  [view addSubview:self.maskView];
+  //  self.maskView.frame = view.bounds;
+  //
+  //  self.frame = CGRectMake(20, OPTScreenHeight, OPTScreenWidth-40, 300);
+  //  [view addSubview:self];
+  //  [view layoutIfNeeded];
+  //
+  //  [UIView animateWithDuration:.3 animations:^{
+  //    self.center = CGPointMake(OPTScreenWidth/2.0, OPTScreenHeight/2.0);
+  //  }];
+  //
+  //  self.showing = YES;
   
   if (self.showing) {
     return;
@@ -68,17 +68,15 @@
     [view addSubview:self];
   }
   
-  [self mas_updateConstraints:^(MASConstraintMaker *make) {
-    make.leading.equalTo(self.superview).with.offset(10);
-    make.trailing.equalTo(self.superview).with.offset(-10);
+  [self mas_makeConstraints:^(MASConstraintMaker *make) {
+    make.leading.trailing.equalTo(self.superview);
     make.top.equalTo(self.superview.mas_bottom).priorityLow();
-    make.height.mas_equalTo(216.f);
+    make.size.mas_equalTo(CGSizeMake(OPTScreenWidth, 200));
   }];
   
   [view layoutIfNeeded];
-  
-  [self mas_updateConstraints:^(MASConstraintMaker *make) {
-    make.bottom.equalTo(self.superview.mas_bottom).with.offset(-3*10);
+  [self mas_makeConstraints:^(MASConstraintMaker *make) {
+    make.bottom.equalTo(self.superview.mas_bottom).offset(0).priorityMedium();
   }];
   
   [UIView animateWithDuration:.3
@@ -89,37 +87,45 @@
 }
 
 - (void)dismiss {
-//  if (!self.showing) {
-//    return;
-//  }
-//  
-//  self.maskView.enabled = NO;
-//  
-//  [self.maskView removeFromSuperview];
-//  self.maskView = nil;
-//  [self removeFromSuperview];
-//  self.showing = NO;
+  //  if (!self.showing) {
+  //    return;
+  //  }
+  //
+  //  self.maskView.enabled = NO;
+  //
+  //  [self.maskView removeFromSuperview];
+  //  self.maskView = nil;
+  //  [self removeFromSuperview];
+  //  self.showing = NO;
   
   if (!self.showing) {
     return;
   }
+  self.maskView.enabled = NO;
   
-  [self mas_remakeConstraints:^(MASConstraintMaker *make) {
-    make.leading.equalTo(self.superview).with.offset(10);
-    make.trailing.equalTo(self.superview).with.offset(-10);
-    make.height.mas_equalTo(216.f);
-    make.top.equalTo(self.superview.mas_bottom).priorityMedium();
+  [self mas_makeConstraints:^(MASConstraintMaker *make) {
+    make.top.equalTo(self.superview.mas_bottom);
   }];
   
-  [UIView animateWithDuration:.3 animations:^{
-    [self layoutIfNeeded];
-  } completion:^(BOOL finished){
-    [self.maskView removeFromSuperview];
-    self.maskView = nil;
-    [self removeFromSuperview];
-  }];
+  [UIView animateWithDuration:.2
+                   animations:^{
+                     [self.superview layoutIfNeeded];
+                   } completion:^(BOOL finished){
+                     [self removeFromSuperview];
+                     [self.maskView removeFromSuperview];
+                     self.maskView.enabled = YES;
+                   }];
   
   self.showing = NO;
+}
+
+- (UIButton *)maskView {
+  if (!_maskView) {
+    _maskView = [UIButton new];
+    _maskView.backgroundColor = [[UIColor blackColor]  colorWithAlphaComponent:.4];
+    [_maskView addTarget:self action:@selector(dismiss) forControlEvents:UIControlEventTouchUpInside];
+  }
+  return _maskView;
 }
 
 @end
